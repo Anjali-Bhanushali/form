bd59f753cdc2674cfdc0284cf6c1c86a3e6f9d71
Anjali kumari <anjalisinghbhanushali@gmail.com>
2026-02-01 20:18:36 +0530

    tailwind ,form basic ui  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className='h-screen flex items-center justify-center'>App
+       <div className='bg-white rounded-xl p-6 w-96'>
+          <form className='flex flex-col items-enter gap-2'>
+            <input className=' w-full border-2 border-black px-3 py-1 text-xl rounded' type="text" placeholder='Enter name here' />
+            <input className=' w-full border-2 border-black px-3 py-1 text-xl rounded' type="email" placeholder='Enter your email'/>
+            <input className=' w-full border-2 border-black px-3 py-1 text-xl rounded' type="password" placeholder='Enter password'/>
+            <input className=' w-full border-2 border-black px-3 py-1 text-xl rounded' type="password" placeholder='Confirm password'/>
+             <button className='text-xl px-4 py-2 rounded bg-emerald-600 font-semibold mt-5 w-full'>Submit</button>
+          </form>
+       </div>
+    </div>
   )
 }
+ 
+
+
 
 export default App
